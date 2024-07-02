5843df12aeb9b77ddda986d0b8b942865a72f2b1
Zhuo Li <130893474+lilizhuogithub@users.noreply.github.com>
2024-07-02 14:47:55 +0800

    套餐分页查询

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,10 +1,14 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Service;
 
 
 public interface SetmealService {
     void save(SetmealDTO setmealDTO);
+
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
