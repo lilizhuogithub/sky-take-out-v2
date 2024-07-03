81944b2841f97dd931da4cb61f0606de18a82bac
Zhuo Li <130893474+lilizhuogithub@users.noreply.github.com>
2024-07-03 17:19:52 +0800

    店铺营业状态设置

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,47 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ *
+ */
+
+
+@RestController("adminShopController")
+@Slf4j
+@RequestMapping("/admin/shop")
+@Api(tags = "店铺相关接口")
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    private static final String KEY = "SHOP_STATUS";
+
+    @PutMapping("/{status}")
+    @ApiOperation("店铺营业状态设置")
+    public Result setStatus(@PathVariable Integer status) {
+        log.info("设置店铺营业状态：{}", status == 1 ? "营业中" : "打样中");
+
+        redisTemplate.opsForValue().set(KEY, status);
+
+        return Result.success();
+    }
+
+    @GetMapping("/status")
+    @ApiOperation("店铺营业状态获取")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("设置店铺营业状态：{}", status == 1 ? "营业中" : "打样中");
+        return Result.success(status);
+    }
+
+
+
+}
