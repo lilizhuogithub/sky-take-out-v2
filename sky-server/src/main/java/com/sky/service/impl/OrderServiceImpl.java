1f0ca32363bd0fa891069bce1e7f303db6a533c1
Zhuo Li <130893474+lilizhuogithub@users.noreply.github.com>
2024-07-16 10:17:01 +0800

    商家端订单管理模块

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -22,10 +22,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -268,6 +270,7 @@ public class OrderServiceImpl implements OrderService {
             shoppingCart.setUserId(userId);
             shoppingCart.setCreateTime(LocalDateTime.now());
 
+
             return shoppingCart;     //将创建并初始化的购物车对象返回给map方法，以便最终收集到shoppingCartList列表中
         }).collect(Collectors.toList());     //.collect方法将转换后的购物车对象收集回一个列表；
 
@@ -275,4 +278,51 @@ public class OrderServiceImpl implements OrderService {
         shoppingCartMapper.insertBatch(shoppingCartList);
     }
 
+    @Override
+    public PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO) {
+        PageHelper.startPage(ordersPageQueryDTO.getPage(), ordersPageQueryDTO.getPageSize());
+
+        Page<Orders> page = orderMapper.pageQuery(ordersPageQueryDTO);
+
+        //部分订单状态,需要额外返回订单菜品信息,将Orders转化为OrderVO
+        List<OrderVO> orderVOList = getOrderVOList(page);
+
+        return new PageResult(page.getTotal(), orderVOList);
+    }
+
+    private List<OrderVO> getOrderVOList(Page<Orders> page) {
+        //需要返回订单菜品信息,自定义OrderVO响应结果
+        List<OrderVO> orderVOList = new ArrayList<>();
+
+        List<Orders> orderList = page.getResult();
+        if (!CollectionUtils.isEmpty(orderList)) {
+            for (Orders orders : orderList) {
+                //将共同字段复制到OrderVO
+                OrderVO orderVO = new OrderVO();
+                BeanUtils.copyProperties(orders, orderVO);
+                String orderDishes = getOrderDishesStr(orders);
+
+                //将订单菜品信息封装到orderVO中,并添加到orderVOList
+                orderVO.setOrderDishes(orderDishes);
+                orderVOList.add(orderVO);
+
+            }
+        }
+        return orderVOList;
+    }
+
+    //根据订单id获取菜品信息字符串
+    private String getOrderDishesStr(Orders orders) {
+        //查看订单菜品详情信息(订单中的菜品和数量)
+        List<OrderDetail> orderDetailList = orderDetailMapper.getByOrderId(orders.getId());
+
+        //将每一条订单菜品信息拼接为字符串(格式:宫保鸡丁*3)
+        List<String> orderDishList = orderDetailList.stream().map(x -> {
+            String orderDish = x.getName() + "*" + x.getNumber() + ";";
+            return orderDish;
+        }).collect(Collectors.toList());
+
+        //将该订单对应的所有菜品信息拼接在一起
+        return String.join("", orderDishList);
+    }
 }
