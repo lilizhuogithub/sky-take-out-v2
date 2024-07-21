d2d671975fcffb06d9e12831d8fa46698c352e02
Zhuo Li <130893474+lilizhuogithub@users.noreply.github.com>
2024-07-21 13:41:08 +0800

    营业额数据统计

---
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -0,0 +1,62 @@
+package com.sky.service.impl;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import org.apache.commons.lang.StringUtils;
+import org.apache.poi.util.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ *
+ */
+@Service
+public class ReportServiceImpl implements ReportService {
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Override
+    public TurnoverReportVO getTurnoverStatistic(LocalDate begin, LocalDate end) {
+
+        //建立一个集合用于存放从begin到end范围内的每天的日期
+        List<LocalDate> dataList = new ArrayList<>();
+        dataList.add(begin);
+        while (!begin.equals(end)) {
+            begin = begin.plusDays(1);
+            dataList.add(begin);
+        }
+
+        //存放每日的营业额
+        List<Double> turnoverList = new ArrayList<>();
+        for (LocalDate date : dataList) {
+            //查询date日期对应的营业额数据，营业额指的是：状态“已完成”的订单金额合计
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+            //select sum(amount) from orders where order_time > beginTime and order_Time < endTime and status = 5
+            HashMap map = new HashMap();
+            map.put("begin", beginTime);
+            map.put("end", endTime);
+            map.put("status", Orders.COMPLETED);
+            Double turnover = orderMapper.sumByMap(map);
+
+            turnover = turnover == null ? 0.0 : turnover;    // 当营业为空时，赋值为0；
+
+            turnoverList.add(turnover);
+        }
+
+        return TurnoverReportVO.builder()
+                .dateList(StringUtils.join(dataList, ",")) //将日期按照dateList要求的格式，按照“，”分隔开
+                .turnoverList(StringUtils.join(turnoverList, ","))
+                .build();
+    }
+}
