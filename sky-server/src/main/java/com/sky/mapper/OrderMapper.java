788f9e2983e7a423739abfe89b7d6339f359399c
Zhuo Li <130893474+lilizhuogithub@users.noreply.github.com>
2024-07-21 19:10:48 +0800

    销量排名统计

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
@@ -54,4 +55,6 @@ public interface OrderMapper {
     Double sumByMap(HashMap map);
 
     Integer countByMap(Map map);
+
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
 }
